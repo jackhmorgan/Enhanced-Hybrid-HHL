d41022c126cc8b6c28907c3976daf139378be59a
jackhmorgan <117677338+jackhmorgan@users.noreply.github.com>
2024-02-29 18:07:41 -0600

    added the sampler get result option

---
+++ examples/swap_test_interpreter.py
@@ -0,0 +1,17 @@
+from qiskit_ibm_runtime import QiskitRuntimeService
+import numpy as np
+
+service = QiskitRuntimeService()
+
+
+
+def st_post_processing(job_id):
+    result = service.job(job_id=job_id).result().quasi_dists[0]
+    if not result==None:
+        counts_01 = result[1]
+        counts_11 = result[3]
+    prob_0 = counts_01/(counts_01+counts_11)
+    return np.sqrt(2*prob_0 - 1)
+
+fidelity = st_post_processing(job_id='cnghjhmj1613i2k3dke0')
+print(np.sqrt(2*(1-fidelity)))
