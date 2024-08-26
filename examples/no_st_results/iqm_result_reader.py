21b32b8e7fff082ed74d19d985a2cb57fe7f19a8
jackhmorgan <117677338+jackhmorgan@users.noreply.github.com>
2024-08-25 21:16:48 -0500

    add sorted no st results to main

---
+++ examples/no_st_results/iqm_result_reader.py
@@ -0,0 +1,49 @@
+import json
+import os
+import numpy as np
+
+from enhanced_hybrid_hhl import QuantumLinearSystemSolver, QuantumLinearSystemProblem
+
+file_name = 'iqm_deneb_nst.json'
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Define the file path
+file_path = os.path.join(script_dir, file_name)
+with open(file_path, 'r') as file:
+    json_data = json.load(file)
+
+problem_list = [QuantumLinearSystemProblem(A_matrix=problem, b_vector=[1,0]) for problem in json_data['problem_list']]
+
+canonical_results = json_data['canonical_results']
+canonical_fidelities = []
+
+hybrid_results = json_data['hybrid_results']
+hybrid_fidelities = []
+
+enhanced_results = json_data['enhanced_results']
+enhanced_fidelities = []
+
+
+for i in range(len(json_data['problem_list'])):
+    ideal_x = QuantumLinearSystemSolver(problem=problem_list[i]).ideal_x_statevector.probabilities()
+
+    measured_x = [canonical_results[i]['01'], canonical_results[i]['11']]
+    measured_x /= np.linalg.norm(measured_x)
+    canonical_fidelities.append(np.dot(ideal_x, measured_x))
+
+    measured_x = [hybrid_results[i]['01'], hybrid_results[i]['11']]
+    measured_x /= np.linalg.norm(measured_x)
+    hybrid_fidelities.append(np.dot(ideal_x, measured_x))
+
+    measured_x = [enhanced_results[i]['01'], enhanced_results[i]['11']]
+    measured_x /= np.linalg.norm(measured_x)
+    enhanced_fidelities.append(np.dot(ideal_x, measured_x))
+
+errors = [np.sqrt(2*(1-fidelity)) for fidelity in canonical_fidelities]
+print('ave can error : ',np.average(errors), '+- ', np.std(errors))
+
+errors = [np.sqrt(2*(1-fidelity)) for fidelity in hybrid_fidelities]
+print('ave hybrid error : ',np.average(errors), '+- ', np.std(errors))
+
+errors = [np.sqrt(2*(1-fidelity)) for fidelity in enhanced_fidelities]
+print('ave enhanced error : ',np.average(errors), '+- ', np.std(errors))
